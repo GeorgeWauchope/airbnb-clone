93f8688646a1e19f2540f265b3261f448443f3be
George Wauchope <69352286+GeorgeWauchope@users.noreply.github.com>
2021-04-15 11:17:52 -0500

    everything complete

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,31 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
-import LanguageIcon from '@material-ui/icons/Language';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import PersonOutlineSharpIcon from '@material-ui/icons/PersonOutlineSharp';
-
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonOutlineSharpIcon from "@material-ui/icons/PersonOutlineSharp";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-logo.jpg"
-        className="header_icon"
-        alt="header image of homes"
-      />
+      <Link to="/">
+        <img
+          src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-logo.jpg"
+          className="header_icon"
+          alt="header image of homes"
+        />
+      </Link>
+
       <div className="header_center">
         <input type="text"></input>
         <SearchIcon />
       </div>
       <div className="header_right">
-          <p>Become a host</p>
-          <LanguageIcon />
-          <ExpandMoreIcon />
-          <PersonOutlineSharpIcon />
-
-
-
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <PersonOutlineSharpIcon />
       </div>
     </div>
   );
